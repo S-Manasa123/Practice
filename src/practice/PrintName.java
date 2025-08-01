f9db0f33709805c8d24c67784bd4822460a33cc0
manasa <manasa.sappani@gmail.com>
2025-08-01 13:29:24 +0530

    here I am taking the input from user

--- src/practice/PrintName.java
+++ src/practice/PrintName.java
@@ -1,10 +1,14 @@
 package practice;
 
+import java.util.Scanner;
+
 public class PrintName {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("manasa");
+		Scanner o=new Scanner(System.in);
+				String string=o.next();
+		System.out.println(string);
 
 	}
 
